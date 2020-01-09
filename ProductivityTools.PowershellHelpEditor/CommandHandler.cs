ed771c08da2cc465fe02479752f7b1b2f4dbff3a
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-01-09 23:02:48 +0100

    initial commit

---
+++ ProductivityTools.PowershellHelpEditor/CommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ProductivityTools.PowershellHelpEditor
+{
+    public class CommandHandler : ICommand
+    {
+        private Action action;
+        private Func<bool> canExecute;
+        public CommandHandler(Action action, Func<bool> canExecute)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
---
+++ ProductivityTools.PowershellHelpEditor/MainVM.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ProductivityTools.PowershellHelpEditor
+{
+    public class MainVM
+    {
+        public ICommand OpenAboutWindowCommand { get; }
+        public ICommand OpeFileDialogCommand { get; }
+
+        public MainVM()
+        {
+            this.OpenAboutWindowCommand = new CommandHandler(OpenAbout, () => true);
+            this.OpeFileDialogCommand = new CommandHandler(OpenFileDialog, () => true);
+        }
+
+        private void OpenAbout()
+        {
+            About about = new About();
+            about.ShowDialog();
+        }
+        
+        private void OpenFileDialog()
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            CommonFileDialogResult result = dialog.ShowDialog();
+        }
+    }
+}
